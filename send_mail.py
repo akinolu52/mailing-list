72023ef15394633f9fbd7b8d06d5e8902ee66a3f
Akinyemi Emmanuel <akinolu52@gmail.com>
2017-09-16 14:31:14 +0100

    some changes

--- send_mail.py
+++ send_mail.py
@@ -5,7 +5,8 @@ def send_email(emails, schedule, forecast):
     server = smtplib.SMTP('smtp.gmail.com', '587')
     server.starttls()
 
-    from_email = 'akinolu52@gmail.com'
+    # from_email = 'akinolu52@gmail.com'
+    from_email = input("Enter your email address: ")
     from_password = input("Enter your password: ")
     server.login(from_email, from_password)
 
